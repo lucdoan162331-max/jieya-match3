730524864b751c144abf4b62031ca9f7e5304d99
lucdoan162331-max <lucdoan162331@gmail.com>
2026-07-10 18:05:22 +0800

    拔高：修连消bug、放大棋盘、轻快BGM、蛋仔风视觉、通关喝彩

--- js/rewards.js
+++ js/rewards.js
@@ -1,7 +1,7 @@
 /**
  * 心理学奖励机制 — 可变比率强化 + 进度里程碑 + 连击多巴胺
  */
-import { randomSfxLine, showSfxToast, playWin } from './audio.js?v=20260710d';
+import { randomSfxLine, showSfxToast, playWin } from './audio.js?v=20260710e';
 
 const COMBO_LINES = [
   '', 'Nice!', 'Double!', 'Triple!!', 'Ultra!!!', 'LEGEND!!!!',
